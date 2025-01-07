ead24c9e0f686e33b28f36ec9f4eaeddb5ade1d4
Ablanin bebesi <128322056+AblaninBebesi@users.noreply.github.com>
2025-01-07 23:38:39 +0300

    Update socket.js

--- client/src/config/socket.js
+++ client/src/config/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'
-const ENDPOINT = "http://127.0.0.1:8080";
+const ENDPOINT = "https://sdokh.onrender.com";
 const socket = io(ENDPOINT, {
     transports: ['websocket'],
     withCredentials: true
